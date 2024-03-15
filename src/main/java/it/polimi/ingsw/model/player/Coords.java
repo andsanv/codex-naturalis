646ab5fadbbda8110484257b26f5ae8e26c5902d
andsanv <sanvito.andrea@yahoo.com>
2024-03-15 18:59:41 +0100

    [Model] Added Coords class and CSV of starter cards

---
+++ src/main/java/it/polimi/ingsw/model/player/Coords.java
@@ -0,0 +1,22 @@
+package it.polimi.ingsw.model.player;
+
+/**
+ *
+ */
+final class Coords {
+    public final int x;
+    public final int y;
+
+    public Coords(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+}
