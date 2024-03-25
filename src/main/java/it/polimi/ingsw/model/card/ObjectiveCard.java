49d21283d95cf693a060eab08c4a14ca9bdc3199
spische <pische.99@hotmail.it>
2024-03-25 11:47:01 +0100

    [Model] Added initial draft of a strategy pattern for objective cards

--- src/main/java/it/polimi/ingsw/model/card/ObjectiveCard.java
+++ src/main/java/it/polimi/ingsw/model/card/ObjectiveCard.java
@@ -5,10 +5,43 @@ package it.polimi.ingsw.model.card;
  * Each player has a personal objective, only visible to him, and all the players share two common objectives, visible by everyone.
  */
 public class ObjectiveCard extends Card {
-    // TODO maybe add a labda to compute objective score
-
+    // TODO maybe add a lambda to compute objective score
     // TODO Create objective class and logic and getter
-    public ObjectiveCard(int id){
+
+    private PointCalculator pointCalculator;
+
+    public ObjectiveCard(int id, PointCalculator pointCalculator) {
         super(id);
+        this.pointCalculator = pointCalculator;
+    }
+
+    public int calculatePoints() {
+        return pointCalculator.calculatePoints();
+    }
+}
+
+
+//STRATEGY PATTERN
+
+// Interface for the strategy pattern
+interface PointCalculator {
+    int calculatePoints();
+}
+
+// Calculate points based on items on a player board
+class ItemsPointCalculator implements PointCalculator {
+    //TODO
+    @Override
+    public int calculatePoints() {
+        return 1;
+    }
+}
+
+// Calculate points based on patterns
+class PatternPointCalculator implements PointCalculator {
+    //TODO
+    @Override
+    public int calculatePoints() {
+        return 2;
     }
 }
