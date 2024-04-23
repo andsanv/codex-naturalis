0b7a829d82aaadbfc29a7fd632452dd2b99bef0a
raveeee0 <raveggigabriele0@gmail.com>
2024-04-23 15:56:39 +0200

    [Network] Added client classes sketch
    Co-authored-by: angpre <angelo02prete@gmail.com>

---
+++ src/main/java/it/polimi/ingsw/network/LobbyHandler.java
@@ -0,0 +1,38 @@
+package it.polimi.ingsw.network;
+
+import java.util.List;
+
+public interface LobbyHandler {
+    Lobby createLobby();
+
+    void joinLobby(int lobbyId);
+
+    void startGame();
+
+    List<Lobby> getAvailableLobbies();
+
+    void reconnectToGame(int lobbyId);
+
+    User signUp(String username, String password);
+
+    boolean login(String password);
+
+}
+
+
+class Lobby {
+    private int id;
+    private User creator;
+    private List<User> users;
+    boolean ongoing;
+}
+
+class User {
+    private String name;
+    private int id;
+
+    @Override
+    public String toString() {
+        return name + "#" + id;
+    }
+}
