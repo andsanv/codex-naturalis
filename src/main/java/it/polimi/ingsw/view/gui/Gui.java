e3be0d5d0296cff4a3515420089cafab1990c960
spische <pische.99@hotmail.it>
2024-05-14 17:54:00 +0200

    [Gui] Added ExecutorService for future server commands handle, created main menu and nickname input page.

--- src/main/java/it/polimi/ingsw/view/gui/Gui.java
+++ src/main/java/it/polimi/ingsw/view/gui/Gui.java
@@ -1,11 +1,30 @@
 package it.polimi.ingsw.view.gui;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import it.polimi.ingsw.view.gui.controllers.MainMenu;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+import java.io.IOException;
 
 public class Gui extends Application {
+
+    private ExecutorService executorService = Executors.newFixedThreadPool(2);
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -17,8 +36,78 @@ public class Gui extends Application {
             Scene scene = new Scene(fxmlLoader.load());
             primaryStage.setScene(scene);
             primaryStage.show();
+
+            ServerEventHandlerTask serverEventHandlerTask = new ServerEventHandlerTask();
+            executorService.submit(serverEventHandlerTask);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+        // shut down the executor service so that the application can exit
+        executorService.shutdownNow();
+    }
+
+    @FXML
+    private VBox VboxCentral;
+
+    @FXML
+    private GridPane gridPane;
+
+    @FXML
+    private Text nicknameText;
+
+    @FXML
+    private TextField nicknameTextField;
+
+    @FXML
+    private Button submitNickname;
+
+    @FXML
+    private void handleSubmitClick(MouseEvent event) {
+        try {
+            String nickname = nicknameTextField.getText();
+            if (!nickname.isEmpty()) {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/gui/mainMenu.fxml"));
+                Parent root = fxmlLoader.load();
+                MainMenu mainMenu = fxmlLoader.getController();
+                mainMenu.Setup(nickname);
+                Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void handleEnterKeyPressed(KeyEvent event) {
+        try {
+            if (event.getCode() == KeyCode.ENTER) {
+                String nickname = nicknameTextField.getText();
+                if (!nickname.isEmpty()) {
+                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/gui/mainMenu.fxml"));
+                    Parent root = fxmlLoader.load();
+                    MainMenu mainMenu = fxmlLoader.getController();
+                    mainMenu.Setup(nickname);
+                    Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+                    Scene scene = new Scene(root);
+                    stage.setScene(scene);
+                    stage.show();
+                }
+                nicknameTextField.clear();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
