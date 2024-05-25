1a96d49659981714e0f2a459db86777ba3cccc3f
spische <pische.99@hotmail.it>
2024-05-25 11:11:00 +0200

    [Gui] Added and fixed placing card in cell

--- src/main/java/it/polimi/ingsw/view/gui/controllers/GameView.java
+++ src/main/java/it/polimi/ingsw/view/gui/controllers/GameView.java
@@ -1,26 +1,23 @@
 package it.polimi.ingsw.view.gui.controllers;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.HPos;
 import javafx.geometry.Pos;
+import javafx.geometry.VPos;
+import javafx.scene.layout.Priority;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.transform.Scale;
 
-import java.awt.event.MouseEvent;
 import java.io.IOException;
-import java.util.Objects;
-import java.util.Stack;
 
 public class GameView {
 
@@ -61,15 +58,10 @@ public class GameView {
             }
         }
 
+        //Create grid images containers
         for (int i = 0; i <=41; i++) {
             for (int j = 0; j <= 41; j++) {
                 StackPane cell = new StackPane();
-                cell.setMinHeight(331);
-                cell.setPrefHeight(331);
-                cell.setMaxHeight(331);
-                cell.setMinHeight(496.5);
-                cell.setPrefHeight(496.5);
-                cell.setMaxHeight(496.5);
                 ImageView imageView = new ImageView();
                 imageView.setFitHeight(331);
                 imageView.setFitWidth(496.5);
@@ -77,13 +69,21 @@ public class GameView {
                 if (i==1 && j==1) {
                     Image image = new Image("/images/cards/fronts/001.png");
                     imageView.setImage(image);
+                    StackPane.setAlignment(imageView, javafx.geometry.Pos.CENTER);
                 }
                 cell.getChildren().add(imageView);
+                gridPane.add(cell, i, j);
+                GridPane.setHalignment(cell, HPos.CENTER); // Horizontal alignment
+                GridPane.setValignment(cell, VPos.CENTER); // Vertical alignment
+                GridPane.setHgrow(cell, Priority.NEVER);  // Horizontal grow
+                GridPane.setVgrow(cell, Priority.NEVER); // Vertical grow
             }
         }
 
+
+        //make grid cells light up
         for (Node node : gridPane.getChildren()) {
-            if (node instanceof ImageView) {
+            if (node instanceof StackPane) {
                 node.setOnMouseEntered(mouseEvent -> {
                     try {
                         boardCellHoverEnter(mouseEvent);
@@ -100,6 +100,32 @@ public class GameView {
                 });
             }
         }
+
+        for (Node node : gridPane.getChildren()) {
+            if (node instanceof StackPane) {
+                StackPane stackPane = (StackPane) node;
+                System.out.println("StackPane Is a children of gridPane");
+
+                // Check if the stackPane has any children
+                if (!stackPane.getChildren().isEmpty() && stackPane.getChildren().get(0) instanceof ImageView) {
+                    System.out.println("ImageView Is a children of stackPane");
+                    stackPane.setOnMouseClicked(mouseEvent -> {
+                        try {
+                            setCardOnClick(mouseEvent);
+                        } catch (IOException e) {
+                            // Log the error with a message
+                            e.printStackTrace();
+                        }
+                    });
+                } else {
+                    System.out.println("StackPane does not contain an ImageView as the first child.");
+                }
+            }
+            else {
+                System.out.println("Node is not an instance of StackPane or Group it is:" + node);
+            }
+        }
+
 
 
 
@@ -163,7 +189,6 @@ public class GameView {
 
     @FXML
     public void handCardHoverEnter(javafx.scene.input.MouseEvent mouseEvent) throws IOException {
-        System.out.println("Mouse entered ImageView");
         StackPane sourceImageView = (StackPane) mouseEvent.getSource();
         sourceImageView.setStyle("-fx-border-color: blue; -fx-border-width: 10;");
         sourceImageView.applyCss();
@@ -171,7 +196,6 @@ public class GameView {
 
     @FXML
     public void handCardHoverExit(javafx.scene.input.MouseEvent mouseEvent) throws IOException {
-        System.out.println("Mouse exited ImageView");
         StackPane sourceImageView = (StackPane) mouseEvent.getSource();
         sourceImageView.setStyle("-fx-border-color: transparent; -fx-border-width: 2;");
         sourceImageView.applyCss();
@@ -179,19 +203,42 @@ public class GameView {
 
     @FXML
     public void boardCellHoverEnter(javafx.scene.input.MouseEvent mouseEvent) throws IOException {
-        StackPane stackPane = new StackPane();
-        stackPane.getChildren().add((ImageView) mouseEvent.getSource());
+        StackPane stackPane = (StackPane) mouseEvent.getSource();
         stackPane.setStyle("-fx-border-color: blue; -fx-border-width: 2;");
         stackPane.applyCss();
     }
 
     @FXML
     public void boardCellHoverExit(javafx.scene.input.MouseEvent mouseEvent) throws IOException {
-
-        StackPane stackPane = new StackPane();
-        stackPane.getChildren().add((ImageView) mouseEvent.getSource());
+        StackPane stackPane = (StackPane) mouseEvent.getSource();
         stackPane.setStyle("-fx-border-color: transparent; -fx-border-width: 2;");
         stackPane.applyCss();
+    }
+
+    @FXML
+    public void setCardOnClick(javafx.scene.input.MouseEvent mouseEvent) throws IOException {
+
+        bringStackPaneToFront(gridPane, (StackPane) mouseEvent.getSource());
+        ImageView imageView = (ImageView) (((StackPane) mouseEvent.getSource()).getChildren().get(0));
+        Image image = new Image("/images/cards/fronts/001.png");
+        imageView.setImage(image);
+        System.out.println("Image set!");
+    }
+
+    public void bringStackPaneToFront(GridPane gridPane, StackPane stackPaneToFront) {
+        // Get the children list of the GridPane
+        ObservableList<Node> children = gridPane.getChildren();
+
+        // Check if the StackPane is in the GridPane
+        if (children.contains(stackPaneToFront)) {
+            // Remove the StackPane from its current position
+            children.remove(stackPaneToFront);
+
+            // Add the StackPane back to the end of the list
+            children.add(stackPaneToFront);
+        } else {
+            System.out.println("The specified StackPane is not a child of the GridPane.");
+        }
     }
 
 
