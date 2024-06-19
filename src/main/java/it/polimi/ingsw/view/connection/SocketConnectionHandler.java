9f291a37eca513914dc309b4e3317c7bf2f0304a
Angelo Prete <angelo.prete@skywarder.eu>
2024-06-19 13:24:10 +0200

    Disabled debug prints

--- src/main/java/it/polimi/ingsw/view/connection/SocketConnectionHandler.java
+++ src/main/java/it/polimi/ingsw/view/connection/SocketConnectionHandler.java
@@ -1,7 +1,14 @@
 package it.polimi.ingsw.view.connection;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 import it.polimi.ingsw.Config;
-import it.polimi.ingsw.controller.server.User;
 import it.polimi.ingsw.distributed.commands.game.GameCommand;
 import it.polimi.ingsw.distributed.commands.main.ConnectionCommand;
 import it.polimi.ingsw.distributed.commands.main.MainCommand;
@@ -13,22 +20,13 @@ import it.polimi.ingsw.distributed.events.main.LobbiesEvent;
 import it.polimi.ingsw.distributed.events.main.MainEvent;
 import it.polimi.ingsw.distributed.events.main.ReconnectToGameEvent;
 import it.polimi.ingsw.distributed.events.main.RefusedConnectionEvent;
-import it.polimi.ingsw.distributed.events.main.UserInfoEvent;
 import it.polimi.ingsw.view.UI;
-
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * This class is the socket implementation of the ConnectionHandler interface.
  */
 public class SocketConnectionHandler extends ConnectionHandler {
-  /** 
+  /**
    * This is the socket instance used for the communication.
    */
   private Socket socket;
@@ -43,6 +41,7 @@ public class SocketConnectionHandler extends ConnectionHandler {
    * this constructor creates a new SocketConnectionHandler.
    * It tries to connect to the server and set the streams.
    * Then calls the createListenerThread function.
+   * 
    * @param userInterface the user interface of the client
    * @throws UnknownHostException
    * @throws IOException
@@ -56,63 +55,67 @@ public class SocketConnectionHandler extends ConnectionHandler {
     this.outputStream.flush();
     this.inputStream = new ObjectInputStream(socket.getInputStream());
 
-    System.out.println("Connected to server and set up streams");
-    
+    // System.out.println("Connected to server and set up streams");
+
     createListenerThread();
 
-    System.out.println("Started listening for events");
+    // System.out.println("Started listening for events");
   }
 
   /**
    * This method keeps listening for events from the server.
-   * Based on the type of the event, it executes the event, except for KeepAliveEvent.
+   * Based on the type of the event, it executes the event, except for
+   * KeepAliveEvent.
    * If a problem occurs, the method quits the loop.
    */
   private void createListenerThread() {
     new Thread(
-            () -> {
-              while (true) {
-                try {
-                  Event event = (Event) inputStream.readObject();
-                  //System.out.println("Received event: " + event);
-                  
-                  if(event instanceof KeepAliveEvent) {
-                    ;
-                  } else if (event instanceof GameEvent) {
-                    GameEvent gameEvent = (GameEvent) event;
-                    gameEvent.execute(userInterface);
-                  } else if (event instanceof MainEvent) {
-                    MainEvent mainEvent = (MainEvent) event;
-                    mainEvent.execute(userInterface);
-                  } else {
-                    System.err.println("Received unknown event");
-                  }
+        () -> {
+          while (true) {
+            try {
+              Event event = (Event) inputStream.readObject();
+              System.out.println("Received event: " + event);
 
-                } catch (IOException e) {
-                  System.err.println("Failed to receive event");
-                  System.err.println("Server probably disconnected");
-
-                  break;
-                } catch (ClassNotFoundException e) {
-                  System.err.println("Failed to decode event");
-                  e.printStackTrace();
-                }
+              if (event instanceof KeepAliveEvent) {
+                ;
+              } else if (event instanceof GameEvent) {
+                GameEvent gameEvent = (GameEvent) event;
+                gameEvent.execute(userInterface);
+              } else if (event instanceof MainEvent) {
+                MainEvent mainEvent = (MainEvent) event;
+                mainEvent.execute(userInterface);
+              } else {
+                // System.err.println("Received unknown event");
               }
-            })
+
+            } catch (IOException e) {
+              // System.err.println("Failed to receive event");
+              // System.err.println("Server probably disconnected");
+
+              break;
+            } catch (ClassNotFoundException e) {
+              // System.err.println("Failed to decode event");
+              e.printStackTrace();
+            }
+          }
+        })
         .start();
   }
 
   /**
    * This method sends a main command to the main server.
-   * The command can be sent only if the client has a UserInfo assigned to it.polimi.ingsw.view.connection
+   * The command can be sent only if the client has a UserInfo assigned to
+   * it.polimi.ingsw.view.connection
+   * 
    * @param command the command to be sent
    * @return true if the command was sent successfully, false otherwise
    */
   @Override
   public boolean sendToMainServer(MainCommand command) {
-    System.out.println("Sending command to main server");
-    if(userInterface.getUserInfo() == null && !(command instanceof ReconnectionCommand) && !(command instanceof ConnectionCommand)) {
-      //System.err.println("User info is null");
+    // System.out.println("Sending command to main server");
+    if (userInterface.getUserInfo() == null && !(command instanceof ReconnectionCommand)
+        && !(command instanceof ConnectionCommand)) {
+      System.err.println("User info is null");
       return false;
     }
     try {
@@ -129,13 +132,14 @@ public class SocketConnectionHandler extends ConnectionHandler {
   /**
    * This method sends a game command to the server.
    * The command can be sent only if the client has a UserInfo assigned to it
+   * 
    * @param command the command to be sent
    * @return true if the command was sent successfully, false otherwise
    */
   @Override
   public boolean sendToGameServer(GameCommand command) {
-    if(userInterface.getUserInfo() == null) {
-      //System.err.println("User info is null");
+    if (userInterface.getUserInfo() == null) {
+      System.err.println("User info is null");
       return false;
     }
     try {
@@ -150,74 +154,77 @@ public class SocketConnectionHandler extends ConnectionHandler {
   }
 
   /**
-   * This method is called when the client wants to try to reconnect to the server.
+   * This method is called when the client wants to try to reconnect to the
+   * server.
    * It sends a ReconnectionCommand to the server and waits for the response.
    * While waiting, it keeps listening for events and puts them in a queue.
-   * Once received a response event (ReconnectToGameEvent, LobbiesEvent, RefusedConnectionEvent), it executes it.
-   * After that, it executes all the events in the queue and finally calls the createListenerThread function.
+   * Once received a response event (ReconnectToGameEvent, LobbiesEvent,
+   * RefusedConnectionEvent), it executes it.
+   * After that, it executes all the events in the queue and finally calls the
+   * createListenerThread function.
+   * 
    * @return true if the reconnection was successful, false otherwise
    */
   @Override
   public boolean reconnect() {
     this.close();
-    
+
     ReconnectionCommand reconnectionCommand = new ReconnectionCommand(userInterface.getUserInfo());
-    
+
     try {
       socket = new Socket(Config.ServerIP, Config.MainSocketPort);
-      System.out.println(socket);
+      // System.out.println(socket);
 
       this.outputStream = new ObjectOutputStream(socket.getOutputStream());
       this.outputStream.flush();
 
       this.inputStream = new ObjectInputStream(socket.getInputStream());
 
-
       sendToMainServer(reconnectionCommand);
 
-      new Thread(()-> {
+      new Thread(() -> {
         Queue<Event> events = new ConcurrentLinkedQueue<>();
-        while(true) {
-          System.out.println("Waiting for events");
+        while (true) {
+          // System.out.println("Waiting for events");
           try {
             Object obj = inputStream.readObject();
-            System.out.println("Received object: " + obj);
+            // System.out.println("Received object: " + obj);
 
             Event event = (Event) obj;
-            System.out.println("Received event (reconnect): " + event);
+            // System.out.println("Received event (reconnect): " + event);
 
-            if(event instanceof ReconnectToGameEvent) {
+            if (event instanceof ReconnectToGameEvent) {
               ReconnectToGameEvent reconnectToGameEvent = (ReconnectToGameEvent) event;
               reconnectToGameEvent.execute(userInterface);
               break;
-            } else if(event instanceof LobbiesEvent) {
+            } else if (event instanceof LobbiesEvent) {
               LobbiesEvent lobbiesEvent = (LobbiesEvent) event;
               lobbiesEvent.execute(userInterface);
               break;
-            } else if(event instanceof RefusedConnectionEvent) {
+            } else if (event instanceof RefusedConnectionEvent) {
               RefusedConnectionEvent refusedConnectionEvent = (RefusedConnectionEvent) event;
               refusedConnectionEvent.execute(userInterface);
               break;
             } else {
               events.add(event);
             }
-            
+
           } catch (IOException e) {
-            System.err.println("Failed to receive event");
-            System.err.println("Server probably disconnected");
+            // System.err.println("Failed to receive event");
+            // System.err.println("Server probably disconnected");
             e.printStackTrace();
 
             break;
           } catch (ClassNotFoundException e) {
-            System.err.println("Failed to decode event");
+            // System.err.println("Failed to decode event");
             e.printStackTrace();
           }
         }
 
-        while(!events.isEmpty()) {
+        while (!events.isEmpty()) {
           Event event = events.poll();
-          System.out.println("Queued event: " + event);
-          if(event instanceof KeepAliveEvent) {
+          // System.out.println("Queued event: " + event);
+          if (event instanceof KeepAliveEvent) {
             ;
           } else if (event instanceof GameEvent) {
             GameEvent gameEvent = (GameEvent) event;
@@ -226,7 +233,7 @@ public class SocketConnectionHandler extends ConnectionHandler {
             MainEvent mainEvent = (MainEvent) event;
             mainEvent.execute(userInterface);
           } else {
-            System.err.println("Received unknown event");
+            // System.err.println("Received unknown event");
           }
           try {
             Thread.sleep(100);
@@ -236,12 +243,12 @@ public class SocketConnectionHandler extends ConnectionHandler {
         }
 
         createListenerThread();
-        
-      }).start();      
+
+      }).start();
 
     } catch (IOException e) {
       e.printStackTrace();
-      System.err.println("Failed to reconnect to server");
+      // System.err.println("Failed to reconnect to server");
       return false;
     }
 
@@ -250,6 +257,7 @@ public class SocketConnectionHandler extends ConnectionHandler {
 
   /**
    * This method is used to close the socket and the streams.
+   * 
    * @return true if the socket was closed successfully, false otherwise
    */
   public boolean close() {
@@ -258,11 +266,11 @@ public class SocketConnectionHandler extends ConnectionHandler {
       inputStream.close();
       socket.close();
     } catch (IOException e) {
-      System.err.println("Failed to close socket");
+      // System.err.println("Failed to close socket");
       return false;
     }
 
-    System.out.println("Closed socket");
+    // System.out.println("Closed socket");
     return true;
   }
 }
