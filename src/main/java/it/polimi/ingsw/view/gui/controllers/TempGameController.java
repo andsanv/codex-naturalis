abddd3d49fe1ecdca748bb3c1137435bb4623c73
andrea <sanvito.andrea@yahoo.com>
2024-06-20 23:58:18 +0200

    [GUI] Added first sketc of support for card placement

--- src/main/java/it/polimi/ingsw/view/gui/controllers/TempGameController.java
+++ src/main/java/it/polimi/ingsw/view/gui/controllers/TempGameController.java
@@ -1,9 +1,14 @@
 package it.polimi.ingsw.view.gui.controllers;
 
+import it.polimi.ingsw.model.card.CardSide;
+import it.polimi.ingsw.model.player.Coords;
+import it.polimi.ingsw.model.player.PlayerToken;
 import it.polimi.ingsw.util.Pair;
 import it.polimi.ingsw.view.gui.GUI;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.ColumnConstraints;
@@ -16,7 +21,12 @@ import javafx.scene.control.ScrollPane;
 public class TempGameController {
     public Pair<Integer, Integer> screenResolution;
     public double screenRatio;
-    public Pair<Double, Double> cardsDimension;
+
+    public Pair<Double, Double> rawCardsDimension;
+    public double cardsRatio;
+    public double targetCardWidth;
+    public Pair<Double, Double> adjustedCardsDimension;
+    public double cardCompressionFactor;
 
     private double zoomScale;
     private double zoomIncrement;
@@ -33,37 +43,50 @@ public class TempGameController {
 
     @FXML
     private GridPane playerBoardGridPane;
+    private double initialGridPaneWidth;
+    private double initialGridPaneHeight;
+    private double currentGridPaneWidth;
+    private double currentGridPaneHeight;
+    private Pair<Integer, Integer> gridCellsCount;
 
     public void initialize(GUI gui) {
         screenResolution = new Pair<>(1440, 900);
         screenRatio =  0.01 * ((double) (100 * screenResolution.first) / screenResolution.second);
-        cardsDimension = new Pair<>(200.0, 100.0);
+
+        rawCardsDimension = new Pair<>(774.0, 397.0);
+        targetCardWidth = 200;
+        cardsRatio = rawCardsDimension.first / rawCardsDimension.second;
+        cardCompressionFactor = targetCardWidth / rawCardsDimension.first;
+        adjustedCardsDimension = new Pair<>(targetCardWidth, targetCardWidth / cardsRatio);
+
+        gridCellsCount = new Pair<>(2, 2);
 
         zoomScale = 1;
         zoomIncrement = 0.1;
 
         initializePlayerBoardGrid();
         initializeScrollPane();
+
+        handlePlayedCardEvent(PlayerToken.RED, 52, CardSide.FRONT, new Coords(0, 0));
     }
 
     public void initializePlayerBoardGrid() {
-        playerBoardGridPane.setPadding(new Insets(cardsDimension.second, cardsDimension.second, cardsDimension.second, cardsDimension.second));
+        playerBoardGridPane.setPadding(new Insets(adjustedCardsDimension.second, adjustedCardsDimension.second, adjustedCardsDimension.second, adjustedCardsDimension.second));
 
-        for(int i = 0; i < 81; i++) {
+        for(int i = 0; i < gridCellsCount.first; i++) {
             RowConstraints row = new RowConstraints();
-            row.setPrefHeight((double) cardsDimension.second);
-            row.setMinHeight((double) cardsDimension.second);
+            row.setPrefHeight((double) adjustedCardsDimension.second);
+            row.setMinHeight((double) adjustedCardsDimension.second);
             playerBoardGridPane.getRowConstraints().add(row);
 
             ColumnConstraints column = new ColumnConstraints();
-            column.setPrefWidth((double) cardsDimension.first);
-            column.setMinWidth((double) cardsDimension.first);
+            column.setPrefWidth((double) adjustedCardsDimension.first);
+            column.setMinWidth((double) adjustedCardsDimension.first);
             playerBoardGridPane.getColumnConstraints().add(column);
-
-            StackPane stackPane = new StackPane();
-
-            playerBoardGridPane.add(stackPane, 0, i);
         }
+
+        initialGridPaneWidth = currentGridPaneWidth = gridCellsCount.first * adjustedCardsDimension.first;
+        initialGridPaneHeight = currentGridPaneHeight = gridCellsCount.second * adjustedCardsDimension.second;
     }
 
     public void initializeScrollPane() {
@@ -100,18 +123,33 @@ public class TempGameController {
         dragStartY = event.getSceneY();
     }
 
+    /**
+     * Handles zooming with "CTRL +" and "CTRL -" commands.
+     *
+     * @param event key pressed KeyEvent
+     */
     @FXML
     private void handleKeyPressedEvent(KeyEvent event) {
         if(!event.isControlDown()) return;
+
+        double zoomFactor = 0;
 
         switch(event.getCode()) {
             case PLUS, EQUALS -> zoom(zoomIncrement);
             case MINUS -> zoom(-zoomIncrement);
         }
+
+        // applyZoom(zoomFactor, mainScrollPane.getViewportBounds().getWidth() / 2, mainScrollPane.getViewportBounds().getHeight() / 2);
     }
 
+    /**
+     * Allows to zoom the selected board.
+     *
+     * @param zoomIncrement zoom factor
+     */
     private void zoom(double zoomIncrement) {
-        if(zoomScale + zoomIncrement < 0.6 || zoomScale + zoomIncrement > 1.4) return;
+        // TODO fix zoom
+        if(zoomScale + zoomIncrement < 0 || zoomScale + zoomIncrement > 2) return;
 
         zoomScale += zoomIncrement;
 
@@ -121,7 +159,22 @@ public class TempGameController {
         playerBoardGridPane.setScaleX(zoomScale);
         playerBoardGridPane.setScaleY(zoomScale);
 
-        mainScrollPane.setHvalue(previousX);
-        mainScrollPane.setVvalue(previousY);
+        double previousGridPaneWidth = currentGridPaneWidth;
+        double previousGridPaneHeight = currentGridPaneHeight;
+
+
+    }
+
+    public void handlePlayedCardEvent(PlayerToken playerToken, Integer cardId, CardSide cardSide, Coords coords) {
+        String path = "images/cards/" + (cardSide == CardSide.FRONT ? "fronts" : "backs")  + "/" + cardId + ".png";
+
+        ImageView card1 = new ImageView();
+        card1.setImage(new Image(path));
+        card1.setScaleX(cardCompressionFactor);
+        card1.setScaleY(cardCompressionFactor);
+
+        playerBoardGridPane.add(card1, 0, 0);
+
+        playerBoardGridPane.getChildren().forEach(x -> System.out.println(x.getClass()));
     }
 }
