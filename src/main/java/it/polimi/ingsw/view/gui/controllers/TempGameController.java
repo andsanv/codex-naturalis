c3d6a4bad8583cf3e173fe7804b1a912973e24dd
andrea <sanvito.andrea@yahoo.com>
2024-06-20 16:48:05 +0200

    [GUI] Re-implemented (still buggy) board zoom

--- src/main/java/it/polimi/ingsw/view/gui/controllers/TempGameController.java
+++ src/main/java/it/polimi/ingsw/view/gui/controllers/TempGameController.java
@@ -1,10 +1,10 @@
 package it.polimi.ingsw.view.gui.controllers;
 
-import it.polimi.ingsw.model.player.Coords;
 import it.polimi.ingsw.util.Pair;
 import it.polimi.ingsw.view.gui.GUI;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
@@ -12,9 +12,6 @@ import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.StackPane;
 import javafx.scene.control.ScrollPane;
 
-
-import java.awt.*;
-import java.sql.SQLOutput;
 
 public class TempGameController {
     public Pair<Integer, Integer> screenResolution;
@@ -40,26 +37,27 @@ public class TempGameController {
     public void initialize(GUI gui) {
         screenResolution = new Pair<>(1440, 900);
         screenRatio =  0.01 * ((double) (100 * screenResolution.first) / screenResolution.second);
-        cardsDimension = new Pair<>(50.0, 100.0);
+        cardsDimension = new Pair<>(200.0, 100.0);
 
         zoomScale = 1;
         zoomIncrement = 0.1;
 
         initializePlayerBoardGrid();
+        initializeScrollPane();
     }
 
     public void initializePlayerBoardGrid() {
-        playerBoardGridPane.setPadding(new Insets(cardsDimension.first, cardsDimension.second, cardsDimension.first, cardsDimension.second));
+        playerBoardGridPane.setPadding(new Insets(cardsDimension.second, cardsDimension.second, cardsDimension.second, cardsDimension.second));
 
-        for(int i = 0; i < 80; i++) {
+        for(int i = 0; i < 81; i++) {
             RowConstraints row = new RowConstraints();
-            row.setPrefHeight((double) 50);
-            row.setMinHeight((double) 50);
+            row.setPrefHeight((double) cardsDimension.second);
+            row.setMinHeight((double) cardsDimension.second);
             playerBoardGridPane.getRowConstraints().add(row);
 
             ColumnConstraints column = new ColumnConstraints();
-            column.setPrefWidth((double) 100);
-            column.setMinWidth((double) 100);
+            column.setPrefWidth((double) cardsDimension.first);
+            column.setMinWidth((double) cardsDimension.first);
             playerBoardGridPane.getColumnConstraints().add(column);
 
             StackPane stackPane = new StackPane();
@@ -68,32 +66,62 @@ public class TempGameController {
         }
     }
 
+    public void initializeScrollPane() {
+        mainScrollPane.setHvalue(0.5);
+        mainScrollPane.setVvalue(0.5);
+    }
 
-
+    /**
+     * Updates start coordinates of an eventual drag action.
+     *
+     * @param event press MouseEvent
+     */
     @FXML
     private void handleOnMousePressed(MouseEvent event) {
         dragStartX = event.getSceneX();
         dragStartY = event.getSceneY();
     }
 
+    /**
+     * Handles dragging mouse on the player board grid.
+     * Calculates translation values, applies them, and updates last drag coordinates.
+     *
+     * @param event drag MouseEvent
+     */
     @FXML
     private void handleMouseDragged(MouseEvent event) {
-        double offsetX = event.getSceneX() - dragStartX;
-        double offsetY = event.getSceneY() - dragStartY;
+        double newViewportX = mainScrollPane.getHvalue() - (event.getSceneX() - dragStartX) / playerBoardGridPane.getWidth();
+        double newViewportY = mainScrollPane.getVvalue() - (event.getSceneY() - dragStartY) / playerBoardGridPane.getHeight();
 
-        double newViewportX = mainScrollPane.getHvalue() - offsetX / playerBoardGridPane.getWidth();
-        double newViewportY = mainScrollPane.getVvalue() - offsetY / playerBoardGridPane.getHeight();
-
-        // Pair<Double, Double> offset = new Pair<>(event.getSceneX() - dragStartCoords.first, event.getSceneY() - dragStartCoords.second);
-        // Pair<Double, Double> newViewportPosition = new Pair<>(mainScrollPane.getHvalue() - offset.first / playerBoardGridPane.getWidth(), mainScrollPane.getVvalue() - offset.second / playerBoardGridPane.getHeight());
-        // TODO CHECK IF USEFUL Pair<Double, Double> maxCoords = new Pair<>(1 - mainScrollPane.getViewportBounds().getWidth() / playerBoardGridPane.getWidth(), 1 - mainScrollPane.getViewportBounds().getHeight() / playerBoardGridPane.getHeight());
-
-        // if(0 <= newViewportPosition.first && newViewportPosition.first < maxCoords.first + 2000)
         mainScrollPane.setHvalue(newViewportX);
-
-        // if(0 <= newViewportPosition.second && newViewportPosition.second < maxCoords.second + 2000)
         mainScrollPane.setVvalue(newViewportY);
 
         dragStartX = event.getSceneX();
-        dragStartY = event.getSceneY();}
+        dragStartY = event.getSceneY();
+    }
+
+    @FXML
+    private void handleKeyPressedEvent(KeyEvent event) {
+        if(!event.isControlDown()) return;
+
+        switch(event.getCode()) {
+            case PLUS, EQUALS -> zoom(zoomIncrement);
+            case MINUS -> zoom(-zoomIncrement);
+        }
+    }
+
+    private void zoom(double zoomIncrement) {
+        if(zoomScale + zoomIncrement < 0.6 || zoomScale + zoomIncrement > 1.4) return;
+
+        zoomScale += zoomIncrement;
+
+        double previousX = mainScrollPane.getHvalue();
+        double previousY = mainScrollPane.getVvalue();
+
+        playerBoardGridPane.setScaleX(zoomScale);
+        playerBoardGridPane.setScaleY(zoomScale);
+
+        mainScrollPane.setHvalue(previousX);
+        mainScrollPane.setVvalue(previousY);
+    }
 }
