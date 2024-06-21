438388e62caf2210df33fd20abf67fc5658320a0
Angelo Prete <angelo.prete@skywarder.eu>
2024-06-21 12:00:37 +0200

    Fixed import error

--- src/main/java/it/polimi/ingsw/distributed/commands/main/KeepAliveCommand.java
+++ src/main/java/it/polimi/ingsw/distributed/commands/main/KeepAliveCommand.java
@@ -1,4 +1,6 @@
-package it.polimi.ingsw.distributed.commands;
+package it.polimi.ingsw.distributed.commands.main;
+
+import it.polimi.ingsw.distributed.commands.Command;
 
 public class KeepAliveCommand extends Command {
     public int lastKnownEventId;
