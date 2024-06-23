5ae6b99cde83099fa34dcb1ae2cd1647de42160d
angpre <angelo02prete@gmail.com>
2024-06-23 16:50:13 +0200

    [Cli tests] Commented out classes since they aren't currently in use

--- src/main/java/it/polimi/ingsw/ClientEntry.java
+++ src/main/java/it/polimi/ingsw/ClientEntry.java
@@ -14,128 +14,128 @@ import java.net.UnknownHostException;
 public class ClientEntry {
   // private final ConnectionHandler connectionHandler;
 
-  public static void main(String[] args) throws Exception {
+  // public static void main(String[] args) throws Exception {
     
-    TUI tui = new TUI();
-    tui.start();
+  //   TUI tui = new TUI();
+  //   tui.start();
 
-  }
+  // }
 
-  public static void rmiTest() throws Exception {
+  // public static void rmiTest() throws Exception {
 
-    CLITest clientEntry = new CLITest();
+  //   CLITest clientEntry = new CLITest();
 
-    RMIConnectionHandler connectionHandler = new RMIConnectionHandler(clientEntry);
-    connectionHandler.sendToMainServer(new ConnectionCommand("rave"));
+  //   RMIConnectionHandler connectionHandler = new RMIConnectionHandler(clientEntry);
+  //   connectionHandler.sendToMainServer(new ConnectionCommand("rave"));
 
-    while (clientEntry.getUserInfo() == null) {
-      try {
-        Thread.sleep(1000);
-      } catch (InterruptedException e) {
-      }
-    }
+  //   while (clientEntry.getUserInfo() == null) {
+  //     try {
+  //       Thread.sleep(1000);
+  //     } catch (InterruptedException e) {
+  //     }
+  //   }
 
-    System.out.println("RMI:" + clientEntry.getUserInfo());
+  //   System.out.println("RMI:" + clientEntry.getUserInfo());
 
-    connectionHandler.sendToMainServer(new CreateLobbyCommand(clientEntry.getUserInfo()));
+  //   connectionHandler.sendToMainServer(new CreateLobbyCommand(clientEntry.getUserInfo()));
 
-    new Thread(() -> {
-      try {
-        socketTest2(new CLITest());
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-    }).start();
+  //   new Thread(() -> {
+  //     try {
+  //       socketTest2(new CLITest());
+  //     } catch (IOException e) {
+  //       e.printStackTrace();
+  //     }
+  //   }).start();
 
     
-    try {
-      Thread.sleep(10000);
-    } catch (InterruptedException e) {
-    }
+  //   try {
+  //     Thread.sleep(10000);
+  //   } catch (InterruptedException e) {
+  //   }
 
-  }
+  // }
 
-  public static void socketTest(CLITest cliTest) throws UnknownHostException, IOException {
+  // public static void socketTest(CLITest cliTest) throws UnknownHostException, IOException {
     
-    SocketConnectionHandler connectionHandler = new SocketConnectionHandler(cliTest);
-    System.out.println("Sending connection command to server");
+  //   SocketConnectionHandler connectionHandler = new SocketConnectionHandler(cliTest);
+  //   System.out.println("Sending connection command to server");
 
-    connectionHandler.sendToMainServer(new ConnectionCommand("rave"));
+  //   connectionHandler.sendToMainServer(new ConnectionCommand("rave"));
     
-    while (cliTest.getUserInfo() == null) {
-      try {
-        Thread.sleep(1000);
-        } catch (InterruptedException e) {
-      }
-    }
+  //   while (cliTest.getUserInfo() == null) {
+  //     try {
+  //       Thread.sleep(1000);
+  //       } catch (InterruptedException e) {
+  //     }
+  //   }
 
-    connectionHandler.sendToMainServer(new CreateLobbyCommand(cliTest.getUserInfo()));
+  //   connectionHandler.sendToMainServer(new CreateLobbyCommand(cliTest.getUserInfo()));
 
-    try {
-      Thread.sleep(10000);
-    } catch (InterruptedException e) {
-    }
+  //   try {
+  //     Thread.sleep(10000);
+  //   } catch (InterruptedException e) {
+  //   }
 
-    connectionHandler.close();
-    System.out.println("Connection closed");
+  //   connectionHandler.close();
+  //   System.out.println("Connection closed");
 
 
-    try {
-      Thread.sleep(5000);
-    } catch (InterruptedException e) {
-    }
-    connectionHandler.reconnect();
-    connectionHandler.sendToMainServer(new CreateLobbyCommand(cliTest.getUserInfo()));
+  //   try {
+  //     Thread.sleep(5000);
+  //   } catch (InterruptedException e) {
+  //   }
+  //   connectionHandler.reconnect();
+  //   connectionHandler.sendToMainServer(new CreateLobbyCommand(cliTest.getUserInfo()));
 
-    try {
-      Thread.sleep(20000);
-    } catch (InterruptedException e) {
-    }
-    connectionHandler.close();
-  }
+  //   try {
+  //     Thread.sleep(20000);
+  //   } catch (InterruptedException e) {
+  //   }
+  //   connectionHandler.close();
+  // }
 
-  public static void socketTest2(CLITest cliTest2) throws UnknownHostException, IOException {
+  // public static void socketTest2(CLITest cliTest2) throws UnknownHostException, IOException {
   
-    System.out.println("How: " + cliTest2.getUserInfo());
-    SocketConnectionHandler connectionHandler = new SocketConnectionHandler(cliTest2);
-    System.out.println("!!!Sending connection command to server");
+  //   System.out.println("How: " + cliTest2.getUserInfo());
+  //   SocketConnectionHandler connectionHandler = new SocketConnectionHandler(cliTest2);
+  //   System.out.println("!!!Sending connection command to server");
 
-    connectionHandler.sendToMainServer(new ConnectionCommand("rave"));
-    System.out.println("!!!Waiting for user info");
-    System.out.println(cliTest2.getUserInfo());
+  //   connectionHandler.sendToMainServer(new ConnectionCommand("rave"));
+  //   System.out.println("!!!Waiting for user info");
+  //   System.out.println(cliTest2.getUserInfo());
     
-    while (cliTest2.getUserInfo() == null) {
-      try {
-        Thread.sleep(1000);
-        } catch (InterruptedException e) {
-      }
-    }
+  //   while (cliTest2.getUserInfo() == null) {
+  //     try {
+  //       Thread.sleep(1000);
+  //       } catch (InterruptedException e) {
+  //     }
+  //   }
 
-    System.out.println("!!!Sending join lobby command to server");
-    System.out.println(cliTest2.getUserInfo());
-    connectionHandler.sendToMainServer(new JoinLobbyCommand(cliTest2.getUserInfo(), 0));
+  //   System.out.println("!!!Sending join lobby command to server");
+  //   System.out.println(cliTest2.getUserInfo());
+  //   connectionHandler.sendToMainServer(new JoinLobbyCommand(cliTest2.getUserInfo(), 0));
 
-    try {
-      Thread.sleep(5000);
-    } catch (InterruptedException e) {
-    }
+  //   try {
+  //     Thread.sleep(5000);
+  //   } catch (InterruptedException e) {
+  //   }
 
-    connectionHandler.close();
-    System.out.println("Connection closed");
+  //   connectionHandler.close();
+  //   System.out.println("Connection closed");
 
 
-    try {
-      Thread.sleep(5000);
-    } catch (InterruptedException e) {
-    }
-    connectionHandler.reconnect();
+  //   try {
+  //     Thread.sleep(5000);
+  //   } catch (InterruptedException e) {
+  //   }
+  //   connectionHandler.reconnect();
 
-    try {
-      Thread.sleep(15000);
-    } catch (InterruptedException e) {
-    }
-    connectionHandler.close();
-  }
+  //   try {
+  //     Thread.sleep(15000);
+  //   } catch (InterruptedException e) {
+  //   }
+  //   connectionHandler.close();
+  // }
 }
 
 
