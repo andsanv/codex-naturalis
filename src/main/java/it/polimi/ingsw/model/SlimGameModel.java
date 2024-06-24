e50972ab4e716f7d7bd561bdc6d37b60f9def3cd
angpre <angelo02prete@gmail.com>
2024-06-24 12:55:47 +0200

    [Deck] Added getter as list of ids method

--- src/main/java/it/polimi/ingsw/model/SlimGameModel.java
+++ src/main/java/it/polimi/ingsw/model/SlimGameModel.java
@@ -6,7 +6,6 @@ import java.util.Map;
 
 import it.polimi.ingsw.model.card.CardSide;
 import it.polimi.ingsw.model.common.Elements;
-import it.polimi.ingsw.model.common.Resources;
 import it.polimi.ingsw.model.player.Coords;
 import it.polimi.ingsw.model.player.PlayerToken;
 import it.polimi.ingsw.util.Pair;
